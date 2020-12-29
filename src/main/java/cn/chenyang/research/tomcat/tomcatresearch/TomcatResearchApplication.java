defa9806da53da5a5e556161a3bc53abfe159ced
chenyang <cheny38@mingyuanyun.com>
2020-12-29 11:00:21 +0800

    first commit

---
+++ src/main/java/cn/chenyang/research/tomcat/tomcatresearch/TomcatResearchApplication.java
@@ -0,0 +1,13 @@
+package cn.chenyang.research.tomcat.tomcatresearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomcatResearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TomcatResearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/chenyang/research/tomcat/tomcatresearch/TomcatResearchApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.chenyang.research.tomcat.tomcatresearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomcatResearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
